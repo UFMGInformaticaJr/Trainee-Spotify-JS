5fa2d33fa83b43c507720d6d5ab99c558e3faceb
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-08-08 19:29:38 -0300

    feat: fazer modelo UserSong
    Co-authored-by: marcelomrad <marcelomrad@users.noreply.github.com>

---
+++ api/src/domains/userSongs/models/UserSong.js
@@ -0,0 +1,40 @@
+const sequelize = require('../../../../database');
+const {DataTypes} = require('sequelize');
+
+const Song = require('../../songs/models/Song.js');
+const User = require('../../users/models/User.js');
+
+const UserSong = sequelize.define('UserSong', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  }
+});
+
+Song.belongsToMany(User, {
+  through: UserSong,
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+User.belongsToMany(Song, {
+  through: UserSong,
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
+
+//Super Many-to-Many
+Song.hasMany(UserSong);
+UserSong.belongsTo(Song);
+User.hasMany(UserSong);
+UserSong.belongsTo(User);
+
+UserSong.sync({alter: true, force: false})
+  .then(() => {
+    console.log('UserSong table was (re)created');
+  })
+  .catch((err) => console.log(err));
+
+module.exports = UserSong;
