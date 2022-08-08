40714e3875791cae7906559163e8184bb04ae4dc
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-08-08 13:18:01 -0300

    feat: objeto com status codes

---
+++ api/constants/statusCodes.js
@@ -0,0 +1,13 @@
+const statusCodes = {
+  success: 200,
+  created: 201,
+  accepted: 202,
+  noContent: 204,
+  badRequest: 400,
+  unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
+  internalServerError: 500,
+};
+
+module.exports = statusCodes;
