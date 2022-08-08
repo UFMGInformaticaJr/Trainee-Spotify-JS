6c743329f1f2b116e8a752bf14c919e94f3a666c
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-08-08 13:17:13 -0300

    feat: model de Song

---
+++ api/src/domains/songs/models/Song.js
@@ -0,0 +1,39 @@
+const sequelize = require('../../../../database');
+const {DataTypes} = require('sequelize');
+
+const Song = sequelize.define('Songs', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cover_image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  artist: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+/*
+Comando para criar/alterar as
+colunas da tabela caso necessário
+ */
+Song.sync({alter: false, force: false})
+  .then(() => {
+    console.log('Song table was (re)created');
+  })
+  .catch((err) => console.log(err));
+
+module.exports = Song;
