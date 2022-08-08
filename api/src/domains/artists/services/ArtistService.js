3760f56b00c06eaeb40034b52c574c9d986e7110
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-08-08 13:15:00 -0300

    feat: service de Artist

---
+++ api/src/domains/artists/services/ArtistService.js
@@ -0,0 +1,58 @@
+const Artist = require('../models/Artist.js');
+const QueryError = require('../../../../errors/QueryError.js');
+
+class ArtistService {
+  async create(body){
+    const artist = {
+      name: body.name,
+      nationality: body.nationality,
+    };
+    await Artist.create(artist);
+  }
+
+  async getAll(){
+    const artists = await Artist.findAll();
+
+    if(!artists){
+      throw new QueryError('Não há nenhum artista cadastrado');
+    }
+
+    return artists;
+  }
+
+  async getById(id){
+    const artist = await Artist.findByPk(id);
+
+    if(!artist){
+      throw new QueryError('Artista não encontrado!');
+    }
+
+    return artist;
+  }
+
+  async updateInfo(id, body){
+    const artist = await Artist.findByPk(id, {attributes:
+        {
+          exclude: ['createdAt', 'updatedAt'],
+        }});
+
+    if(!artist){
+      throw new QueryError('Artista não encontrado!');
+    }
+
+    await artist.update(body);
+  }
+
+  async delete(id){
+    const artist = await Artist.findByPk(id);
+
+    if(!artist){
+      throw new QueryError('Artista não encontrado!');
+    }
+
+    await artist.destroy();
+  }
+
+}
+
+module.exports = ArtistService;
