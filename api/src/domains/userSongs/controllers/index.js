5e027b85b1a09c707dae3544fe42f9993a4809a9
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-08-08 19:29:18 -0300

    feat: fazer controller userSongs

---
+++ api/src/domains/userSongs/controllers/index.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const UserSongService = require('../services/UserSongService.js');
+const {jwtMiddleware} = require('../../../middlewares/auth-middlewares');
+const statusCodes = require('../../../../constants/statusCodes.js');
+
+router.post('/song/:id',
+  jwtMiddleware,
+  async (req, res, next) => {
+    try {
+      await UserSongService.create(req.user.id, req.params.id);
+      res.status(statusCodes.created).end();
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.get('/user/:id',
+  jwtMiddleware,
+  async (req, res, next) => {
+    try{
+      const songs = await UserSongService.getAllSongsByUser(req.params.id);
+      res.status(statusCodes.success).json(songs);
+    }catch (error){
+      next(error);
+    }
+  },
+);
+
+router.get('/song/:id',
+  jwtMiddleware,
+  async (req, res, next) => {
+    try {
+      const users = await UserSongService.getAllUsersBySong(req.params.id);
+      res.status(statusCodes.success).json(users);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.delete('/songs/:id',
+  jwtMiddleware,
+  async (req, res, next) => {
+    try {
+      await SongService.delete(req.params.id);
+      res.sendStatus(statusCodes.noContent);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+module.exports = router;
