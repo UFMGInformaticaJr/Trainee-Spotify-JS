5f70dd2d9372517a14e54520f425be0684395db3
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-08-08 13:14:40 -0300

    feat: model de Artist

--- api/src/domains/artists/models/Artist.js
+++ api/src/domains/artists/models/Artist.js
@@ -1,6 +1,28 @@
-const sequelize = require('../../database');
+const sequelize = require('../../../../database');
 const {DataTypes} = require('sequelize');
 
 const Artist = sequelize.define('Artist', {
-    
-})
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  nationality: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+
+});
+
+Artist.sync({alter: false, force: false})
+  .then(() => {
+    console.log('Artist table was (re)created');
+  })
+  .catch((err) => console.log(err));
+
+module.exports = Artist;
