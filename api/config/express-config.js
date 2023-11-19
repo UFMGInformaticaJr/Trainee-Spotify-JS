d28ab5259fc062cda28e68821a1830351d24f4ef
Carlos Magalhães Silva <carlosmsilva755@gmail.com>
2023-11-19 14:43:33 -0300

    fix: alteração nas corsOptions

--- api/config/express-config.js
+++ api/config/express-config.js
@@ -1,38 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-app.use(cors(
-  {
-    origin: process.env.CLIENT_URL,
+const cors = require("cors");
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3030",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173:3030",
+      "http://127.0.0.1:5173",
+      "http://localhost:3000",
+    ],
     credentials: true,
-  },
-));
+  })
+);
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-app.use(express.urlencoded({
-  extended: true,
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.json());
 
-const usersRouter = require('../src/domains/users/controllers/index.js');
-app.use('/api/users', usersRouter);
+const usersRouter = require("../src/domains/users/controllers/index.js");
+app.use("/api/users", usersRouter);
 
-const artistsRouter = require('../src/domains/artists/controllers/index.js');
-app.use('/api/artists', artistsRouter);
+const artistsRouter = require("../src/domains/artists/controllers/index.js");
+app.use("/api/artists", artistsRouter);
 
-const songsRouter = require('../src/domains/songs/controllers/index.js');
-app.use('/api/songs', songsRouter);
+const songsRouter = require("../src/domains/songs/controllers/index.js");
+app.use("/api/songs", songsRouter);
 
-const usersSongsRouter = require('../src/domains/userSongs/controllers/index.js');
-app.use('/api/users-songs', usersSongsRouter);
+const usersSongsRouter = require("../src/domains/userSongs/controllers/index.js");
+app.use("/api/users-songs", usersSongsRouter);
 
-const errorHandler = require('../src/middlewares/error-handler.js');
+const errorHandler = require("../src/middlewares/error-handler.js");
 app.use(errorHandler);
 
 module.exports = app;
