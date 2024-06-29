85f0338aaf86bb69a03ede257e9e9bfa0ee55a5e
arigsf <arifilhosilva235@gmail.com>
2024-06-29 14:19:45 -0300

    feat: adiciona metodo para remover relacao entre usuario e musica

--- api/src/domains/userSongs/services/UserSongService.js
+++ api/src/domains/userSongs/services/UserSongService.js
@@ -3,12 +3,31 @@ const UserService = require('../../users/services/UserService.js');
 const SongService = require('../../songs/services/SongService.js');
 const Song = require('../../songs/models/Song.js');
 const User = require('../../users/models/User.js');
+const QueryError = require('../../../../errors/QueryError.js');
 
 class UserSongService  {
   async create(userId, songId){
     const user = await UserService.getById(userId);
     const song = await SongService.getById(songId);
     await song.addUser(user.id);
+  }
+
+  async delete(userId, songId){
+    const user = await UserService.getById(userId);
+    const song = await SongService.getById(songId);
+
+    const userSong = await UserSong.findOne({
+      where: {
+        UserId: user.id,
+        SongId: song.id
+      }
+	});
+  
+	if (!userSong) {
+      throw new QueryError('O usuário não ouviu essa música!');
+	}
+
+	await userSong.destroy();
   }
 
   async getAllSongsByUser(userId){
